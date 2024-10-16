2e5e02c4e03e57f47b81d95b716c78b9ca55700f
hedi-code <hedibensafegine7@gmail.com>
2024-10-16 13:28:06 +0300

    Add gallery.

--- src/app/galerie/galerie.component.ts
+++ src/app/galerie/galerie.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './galerie.component.scss'
 })
 export class GalerieComponent {
+  images: string[] = [
+    "assets/galerie/Forum ESTP 44-001.jpg",
+    "assets/galerie/Forum ESTP 44-004.jpg",
+    "assets/galerie/Forum ESTP 44-030.jpg",
+    "assets/galerie/Forum ESTP 44-041.jpg",
+    "assets/galerie/Forum ESTP 44-080.jpg",
+    "assets/galerie/Forum ESTP 44-117.jpg",
+    "assets/galerie/Forum ESTP 44-125.jpg",
+    "assets/galerie/Forum ESTP 44-132.jpg",
+    "assets/galerie/Forum ESTP 44-154.jpg",
+    "assets/galerie/Forum ESTP 44-155.jpg",
+    "assets/galerie/Forum ESTP 44-163.jpg",
+    "assets/galerie/Forum ESTP 44-178.jpg",
+    "assets/galerie/Forum ESTP 44-205.jpg",
+    "assets/galerie/Forum ESTP 44-221.jpg",
+    "assets/galerie/Forum ESTP 44-226.jpg",
+    "assets/galerie/Forum ESTP 44-229.jpg",
+    "assets/galerie/Forum ESTP 44-243.jpg",
+    "assets/galerie/Forum ESTP 44-261.jpg",
+    "assets/galerie/Forum ESTP 44-263.jpg",
+    "assets/galerie/Forum ESTP 44-339.jpg"
+  ]
 
 }
